94946be3f2866df491ffc9ae8f36f99eec47fbd0
Iamdodo22 <asimiazeez493@gail.com>
2023-10-16 07:00:10 +0100

    active page func

--- script.js
+++ script.js
@@ -12,7 +12,42 @@ menuOpenBtn.addEventListener('click', (e)=>{
 menuCloseBtn.addEventListener('click', (e)=>{
     menuBar.style.display='none';
     menuCloseBtn.style.display='none';
-    menuOpenBtn.style.opacity=1;
+    menuOpenBtn.style.opacity=1; 
+})
 
-    
+
+// menubar active link setting
+const menuBtn = document.querySelectorAll('.menu-bar li a');
+
+const activePage = window.location.pathname;
+
+menuBtn.forEach(link=>{
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('active')
+    }
+})
+
+
+// destination active link setting
+const destNav = document.querySelectorAll('.destCon-sub--nav li a')
+destNav.forEach(link=>{
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('active')
+    }
+})
+
+//crew active link setting
+const crewNav = document.querySelectorAll('.crew-navBar li a')
+crewNav.forEach(link=>{
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('activeCrew')
+    }
+})
+
+//tech active link setting
+const techNav = document.querySelectorAll('.techNav  div a')
+techNav.forEach(link=>{
+    if(link.href.includes(`${activePage}`)){
+        link.classList.add('techActive')
+    }
 })
